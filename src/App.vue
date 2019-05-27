57cdb788f70cd441c0fc297fdbda32ffdefadaed
GenrihMD <genrih.md@gmail.com>
2019-05-27 14:45:37 +0300

    markup

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,108 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+    <div id="app">
+        <b-container>
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+                <b-row>
+                    <div class="col-8">
+                        <div class="products">
+                            <li v-for="product in products">
+                              {{ product.name }}
+                            </li>
+                            <b-row>
+                                <b-col cols="6">
+                                    <b-form-group id="input-group-2" label-for="input-2">
+                                        <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
+                                    </b-form-group>
+                                </b-col>
+                                <b-col cols="3">
+                                    <b-form-group id="input-group-2" label-for="input-2">
+                                        <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
+                                    </b-form-group>
+                                </b-col>
+                                <b-col cols="2">
+                                    <b-form-group id="input-group-2" label-for="input-2">
+                                        <b-form-input id="input-2" v-model="form.name" type="number" required placeholder="Enter name"></b-form-input>
+                                    </b-form-group>
+                                </b-col>
+                                <b-col cols="1">
+                                    <b-button type="button" variant="danger">-</b-button>
+                                </b-col>
+                            </b-row>
+                            <b-button type="button" variant="primary">Add</b-button>
 
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <b-form-group id="input-group-2">
+                            <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
+                        </b-form-group>
+                        <b-form-group id="input-group-2">
+                            <b-form-input id="input-2" v-model="form.phone" required placeholder="Enter phone"></b-form-input>
+                        </b-form-group>
+                        <b-form-group id="input-group-1">
+                            <b-form-input id="input-1" v-model="form.email" type="email" required placeholder="Enter email"></b-form-input>
+                        </b-form-group>
+                    </div>
+                </b-row>
+                <b-row>
+                    <b-button type="submit" variant="primary">Submit</b-button>
+                    <b-button type="reset" variant="danger">Reset</b-button>
+                </b-row>
+            </b-form>
+        </b-container>
+        <b-card class="mt-3" header="Form Data Result">
+            <pre class="m-0">{{ form }}</pre>
+        </b-card>
+    </div>
+</template>
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'app',
+    data() {
+        return {
+            form: {
+                email: '',
+                name: '',
+                phone: '',
+                products: {},
+            },
+            show: true
+        }
+    },
+    methods: {
+        onSubmit(evt) {
+            evt.preventDefault()
+            alert(JSON.stringify(this.form))
+        },
+        onReset(evt) {
+            evt.preventDefault()
+            // Reset our form values
+            this.form.email = ''
+            this.form.name = ''
+            this.form.food = null
+            this.form.checked = []
+            // Trick to reset/clear native browser form validation state
+            this.show = false
+            this.$nextTick(() => {
+                this.show = true
+            })
+        }
     }
-  }
 }
 </script>
-
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.products {
+  border: 1px solid #777777;
+  padding: 5px;
+  border-radius: 2px;
 }
 </style>
